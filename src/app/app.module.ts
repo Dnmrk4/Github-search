d7118547d4beea25ca758f21bb830f60d3910ea8
Dnmrk4 <danmark.chemuren@gmail.com>
2019-06-11 12:47:31 +0300

    Added module typescript

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+import { RepoService } from './repo.service';
+import { SearchService } from './search.service';
+import { RoutingModule } from '../routing/routing.module';
+
+import { GithubService } from './github.service';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    HttpClientModule,
+    RoutingModule
   ],
-  providers: [],
+  providers: [GithubService, RepoService, SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
