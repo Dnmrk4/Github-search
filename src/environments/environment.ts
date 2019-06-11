b20d6fc28c37c1b8216e0df942bc19f68a420e3c
Dnmrk4 <danmark.chemuren@gmail.com>
2019-06-11 12:50:04 +0300

    Updated environment typescript

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  acesstoken: '?access_token=d03e48f83a17ff62157a2aac3923db2be6dac482',
+  search: 'https://api.github.com/users/',
+  apiUrl: 'https://api.github.com/users/Dnmrk4/repos',
+  link: 'https://api.github.com/users/Dnmrk4?access_token=724d0b7d877e52ce02c97a7fdecdcdfc2edc715c'
 };
+
 
 /*
  * In development mode, to ignore zone related error stack frames such as
