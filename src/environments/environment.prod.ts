665efc0de98ed8970795ad2acb0a0d82a676b020
Dnmrk4 <danmark.chemuren@gmail.com>
2019-06-11 13:39:26 +0300

    Added png image and some updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  acesstoken: '?access_token=d03e48f83a17ff62157a2aac3923db2be6dac482',
+  acesstoken: '?access_token=e6eb73292a4b2a6b558d6d776d0a6fae90b05d6c',
   search: 'https://api.github.com/users/',
   apiUrl: 'https://api.github.com/users/Dnmrk4/repos',
   link: 'https://api.github.com/users/Dnmrk4?access_token=724d0b7d877e52ce02c97a7fdecdcdfc2edc715c'
