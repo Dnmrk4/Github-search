c4ce96295eb18a3b8fbfa61ee051329200c5d14c
Dnmrk4 <danmark.chemuren@gmail.com>
2019-06-11 14:37:15 +0300

    Updated environment tokens

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  acesstoken: '?access_token=1429bf67d1d27f373f98cfe5f410fb769e1935fd',
+  acesstoken: '?access_token=d941322fbeb8926fcff82e8c9725240d2007d395',
   search: 'https://api.github.com/users/',
   apiUrl: 'https://api.github.com/users/Dnmrk4/repos',
   link: 'https://api.github.com/users/Dnmrk4?access_token=724d0b7d877e52ce02c97a7fdecdcdfc2edc715c'
